9d9183d1d62109cd7dc7261a8218b99818d6770e
LastStap <dumshenko2003@gmail.com>
2025-04-15 13:14:05 +0200

    Happy birthday

---
+++ src/main/java/daniil/dumshenko/Main.java
@@ -0,0 +1,13 @@
+package daniil.dumshenko;
+
+public class Main
+{
+    public static void main( String[] args ) {
+//
+//        String name = "Nikita";
+//
+//        Greeting greeting = new Greeting();
+//
+//        System.out.println(greeting.greeting(name));
+    }
+}
